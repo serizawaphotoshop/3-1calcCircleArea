05d5340314083b14865732e139bf34485d1e6bf6
a-serizawa <81806545+a-serizawa@users.noreply.github.com>
2022-03-05 08:08:37 +0900

    first commit

---
+++ ans.js
@@ -0,0 +1,8 @@
+'use strict';
+function calcCircleArea(radius){
+    return radius**2*3.14;
+}
+
+console.log(`半径 2 の円の面積は ${calcCircleArea(2)} です。`);
+console.log(`半径 5 の円の面積は ${calcCircleArea(5)} です。`);
+console.log(`半径 10 の円の面積は ${calcCircleArea(10)} です。`);
