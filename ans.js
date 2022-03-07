53517d21e39306c59fdab689b5eeaf95425040cd
a-serizawa <81806545+a-serizawa@users.noreply.github.com>
2022-03-07 13:29:09 +0900

    feat:関数のみで解が出るようにしました

--- ans.js
+++ ans.js
@@ -1,8 +1,15 @@
 'use strict';
-function calcCircleArea(radius){
-    return radius**2*3.14;
-}
+// function calcCircleArea(radius){
+//     return radius**2*3.14;
+// }
 
-console.log(`半径 2 の円の面積は ${calcCircleArea(2)} です。`);
-console.log(`半径 5 の円の面積は ${calcCircleArea(5)} です。`);
-console.log(`半径 10 の円の面積は ${calcCircleArea(10)} です。`);
+// console.log(`半径 2 の円の面積は ${calcCircleArea(2)} です。`);
+// console.log(`半径 5 の円の面積は ${calcCircleArea(5)} です。`);
+// console.log(`半径 10 の円の面積は ${calcCircleArea(10)} です。`);
+
+function calcCircleArea(radius){
+    console.log(`半径${radius}の円の面積は${radius**2*3.14}です。`);
+}
+calcCircleArea(2);
+calcCircleArea(5);
+calcCircleArea(10);
